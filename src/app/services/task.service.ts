d279d2a305ba8bb76e1a7c0fda76d65ebec510ae
Stav Dahan <stav0405@gmail.com>
2022-02-14 20:57:51 +0200

    update functionality added

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http'
 import { Task } from '../Task';
 import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 @Injectable({
   providedIn: 'root',
 })
@@ -17,5 +23,10 @@ export class TaskService {
   deleteTask(task: Task): Observable<Task>{
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url)
+  }
+  
+  updateTaskReminder(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions)
    }
 }
